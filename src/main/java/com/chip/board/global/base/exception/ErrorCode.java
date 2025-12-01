5fadc204c8f34245108f000fe4ccb4f941fad01e
jsj0010 <wjd020425@kumoh.ac.kr>
2025-12-01 19:55:48 +0900

    Update ErrorCode.java
    commit test

--- src/main/java/com/chip/board/global/base/exception/ErrorCode.java
+++ src/main/java/com/chip/board/global/base/exception/ErrorCode.java
@@ -24,7 +24,6 @@ public enum ErrorCode {
 
     // User
     USER_NOT_FOUND(NOT_FOUND, "USER_001", "해당 사용자를 찾을 수 없습니다.")
-
     ;
     private final HttpStatus status;
     private final String code;
