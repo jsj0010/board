75e19b092d6c390c33ba1eeac1dc611f9f412206
jsj0010 <wjd020425@kumoh.ac.kr>
2025-12-19 17:00:52 +0900

    fix : jwt버전 상향
    jwt버전 상향

--- src/main/java/com/chip/board/global/jwt/token/access/AccessTokenProvider.java
+++ src/main/java/com/chip/board/global/jwt/token/access/AccessTokenProvider.java
@@ -5,21 +5,20 @@ import com.chip.board.global.jwt.properties.JwtProperties;
 import com.chip.board.register.domain.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 
 public class AccessTokenProvider {
-    private final Key key;
+    private final SecretKey secretKey;
     private final String issuer;
     private final long accessTtlSeconds;
 
     public AccessTokenProvider(JwtProperties props) {
-        this.key = Keys.hmacShaKeyFor(props.secret().getBytes(StandardCharsets.UTF_8));
+        this.secretKey = Keys.hmacShaKeyFor(props.secret().getBytes(StandardCharsets.UTF_8));
         this.issuer = props.issuer();
         this.accessTtlSeconds = props.accessTtlSeconds();
     }
@@ -29,31 +28,29 @@ public class AccessTokenProvider {
         Instant exp = now.plusSeconds(accessTtlSeconds);
 
         String jwt = Jwts.builder()
-                .setIssuer(issuer)
-                .setSubject(String.valueOf(claims.userId()))
-                .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(exp))
+                .issuer(issuer)
+                .subject(String.valueOf(claims.userId()))
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(exp))
                 .claim("typ", "access")
                 .claim("role", claims.role().name())
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(secretKey, Jwts.SIG.HS256)
                 .compact();
-
 
         return new AccessTokenData(jwt, exp);
     }
+
     public JwtClaims parseToken(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(key)          // 토큰 생성 때 사용한 SecretKey
-                .requireIssuer(issuer)       // issuer 검증
+        Claims claims = Jwts.parser()
+                .verifyWith(secretKey)
+                .requireIssuer(issuer)
+                .require("typ", "access")
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
 
         Long userId = Long.valueOf(claims.getSubject());
-        String roleName = claims.get("role", String.class);
-        Role role = Role.valueOf(roleName);
-
+        Role role = Role.valueOf(claims.get("role", String.class));
         return new JwtClaims(userId, role);
     }
-
 }
